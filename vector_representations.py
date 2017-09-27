fix bugs in similarity calculation logic

vector_representations.py
@@ -2,8 +2,9 @@ from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-
+import nltk
 import numpy as np
+from matplotlib import pyplot as plt
 
 
 # divide 20news data into different groups
@@ -20,7 +21,7 @@ def group_data(twenty_train):
 
 
 def construct_matrix_and_group(vectorizer):
-    twenty_train = fetch_20newsgroups(subset="train", shuffle=True)
+    twenty_train = fetch_20newsgroups(subset="all", shuffle=True)
     group_matrix = vectorizer.fit_transform(twenty_train.data)
     groups = group_data(twenty_train)
     return group_matrix, groups
@@ -30,7 +31,7 @@ def calculate_similarity_in_one_group(matrix, groups_map):
     num_of_groups = len(groups_map.keys())
     similarity_matrix = np.ones((num_of_groups, num_of_groups))
     for first in range(num_of_groups):
-        for second in range(first + 1, num_of_groups):
+        for second in range(first, num_of_groups):
             average_similarity = cosine_similarity(matrix[groups_map[first]], matrix[groups_map[second]]).mean()
             similarity_matrix[first][second] = average_similarity
             similarity_matrix[second][first] = average_similarity
@@ -38,17 +39,19 @@ def calculate_similarity_in_one_group(matrix, groups_map):
 
 
 def calculate_similarity_between_groups(matrix, groups_map):
-    group1 = {key: value[:100] for key, value in groups_map.items()}
-    group2 = {key: value[100:] for key, value in groups_map.items()}
-    similarity_matrix = np.ones((len(group1.keys()), len(group2.keys())))
-    for first in group1.keys():
-        for second in group2.keys():
+    num_of_groups = len(groups_map.keys())
+    representative_group = {key: value[:100] for key, value in groups_map.items()}
+    query_group = {key: value[100:] for key, value in groups_map.items()}
+    similarity_matrix = np.ones((len(query_group.keys()), len(representative_group.keys())))
+    for first in range(num_of_groups):
+        for second in range(num_of_groups):
             average_similarity = cosine_similarity(matrix[groups_map[first]], matrix[groups_map[second]]).mean()
             similarity_matrix[first][second] = average_similarity
     return similarity_matrix
 
+
 if __name__ == '__main__':
     vectorizer_1 = CountVectorizer()
     matrix, groups_map = construct_matrix_and_group(vectorizer_1)
-    similarities = calculate_similarity_between_groups(matrix, groups_map)
-    print(similarities)
+    similarity_matrix = calculate_similarity_in_one_group(matrix, groups_map)
+    print(similarity_matrix.max())